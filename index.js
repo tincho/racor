2d2257fbae8bb7a14671665afa80448890a19b5d
tincho <martin.salinas@gmail.com>
2018-06-29 00:01:24 -0300

    unit tests yay!

--- index.js
+++ index.js
@@ -4,67 +4,23 @@ const get = require('lodash/fp/get');
 const set = require('lodash/fp/set');
 const flow = require('lodash/fp/flow');
 
-//console.log(get('test')({test: 14}));
-
 // "HYDRANT"
 // HOSE-FITTING
 // GET-MAP-SET
 // gets data
 // maps paths to values
 
-(function h() {
-    var spec = [{
-        name: '',
-        // key/path to get from response data obj (using lodash/get supported format)
-        getPath: 'proveedores',
-        // mappers (defaults to "identity", i.e. function(a) {return a })
-        // FIXED ARITY OF 1
-        // options:
-        // apply: ['proveedores', 'return proveedores.map(p => p.id)'],
-        apply: proveedores => proveedores.map(p => p.id),
-        // apply: {
-        //     args: 'proveedores',
-        //     body: 'return proveedores.map(p => p.id)'
-        // },
-        // path to set in the output object (using lodash/set supported format)
-        setPath: 'schema.properties.proveedores.items.enum'
-    }];
-
-    var input = {
-        proveedores: [{
-            id: 1414,
-            nombre: "pipi"
-        }, {
-            id: 11,
-            nombre: "pipi 2"
-        }]
-    };
-
-    let toHydrate = {};
-    toHydrate = {
-        schema: {
-            properties: {
-                proveedores: {
-                    type: 'string',
-                    title: 'Sarasa',
-                    default: "1"
-                }
-            }
-        }
-    };
-
-    var obj = hoseFit(spec[0], input, toHydrate);
-
-    console.log(JSON.stringify(obj, null, 2));
-
-    console.log('original:');
-    console.log(JSON.stringify(toHydrate, null, 2));
-
-    // inputValue = get('input, getPath)
-    // value = new Function(apply.args, apply.body)()
-    // set({}, )
-})();
-
+/**
+ * "Hose Fit", in spanish "Acople de mangueras"
+ * Given a "spec": 
+ *  search in "input" a value indexed by "spec.getPath",
+ *  make it go through "apply"
+ *  and set the output to "spec.setPath"
+ * @returns it merged with "output", set
+ * @param {Object} spec 
+ * @param {*} input 
+ * @param {*} output 
+ */
 function hoseFit(spec, input, output = {}) {
     const identity = i => i;
     const ensureFn = fn => {
@@ -72,20 +28,19 @@ function hoseFit(spec, input, output = {}) {
             return fn;
         }
         if (fn.args && fn.body) {
-            fn = [fn.args, fn.body];
+            fn = [ fn.args, fn.body ];
         }
         try {
             return new Function(...fn);
         } catch(e) {
-            console.log(e);
             return identity;
         }
     }
 
-    let { 
+    let {
         getPath,
         apply,
-        setPath 
+        setPath
     } = spec;
     let getR   = get(getPath);
     let applyR = ensureFn(apply);
@@ -94,3 +49,5 @@ function hoseFit(spec, input, output = {}) {
     let setR   = set(setPath, value);
     return setR(output);
 }
+
+module.exports = hoseFit;
---
+++ test.js
@@ -0,0 +1,105 @@
+const assert = require('assert');
+const hoseFit = require('./index');
+
+describe('HoseFit', () => {
+
+    var input = {
+        proveedores: [{
+            id: 1414,
+            nombre: "pipi"
+        }, {
+            id: 11,
+            nombre: "pipi 2"
+        }]
+    };
+
+    describe('basics', () => {
+        it('should get, apply and set', () => {
+            var spec = {
+                getPath: 'proveedores',
+                apply: proveedores => proveedores.map(p => p.id),
+                setPath: 'schema.properties.proveedores.items.enum'
+            };
+            var obj = hoseFit(spec, input);
+            assert.equal(obj.schema.properties.proveedores.items.enum[0], 1414);
+            assert.equal(obj.schema.properties.proveedores.items.enum[1], 11);
+        });
+
+        it('should merge if given an "output" object', () => {
+            var spec = {
+                getPath: 'proveedores',
+                apply: proveedores => proveedores.map(p => p.id),
+                setPath: 'schema.properties.proveedores.items.enum'
+            };
+            let toHydrate = {
+                schema: {
+                    properties: {
+                        proveedores: {
+                            type: 'string',
+                            title: 'Sarasa',
+                            default: "1"
+                        }
+                    }
+                }
+            };
+            var obj = hoseFit(spec, input, toHydrate);
+            assert.equal(obj.schema.properties.proveedores.items.enum[0], 1414);
+            assert.equal(obj.schema.properties.proveedores.items.enum[1], 11);
+            // and also assert that was merged to the given object toHydrate
+            assert.equal(obj.schema.properties.proveedores.title, 'Sarasa');
+        });
+
+        it('should not mutate the given "output" object but return a new one', () => {
+            // this test actually is redundantly covering lodash's "set" function
+            // but I want to double-check the purity of my function
+            var spec = {
+                getPath: 'proveedores',
+                apply: proveedores => proveedores.map(p => p.id),
+                setPath: 'schema.properties.proveedores.items.enum'
+            };
+            let toHydrate = {
+                schema: {
+                    properties: {
+                        proveedores: {
+                            type: 'string',
+                            title: 'Sarasa',
+                            default: "1"
+                        }
+                    }
+                }
+            };
+            var obj = hoseFit(spec, input, toHydrate);
+            assert.deepEqual(Object.keys(toHydrate.schema.properties.proveedores), ['type', 'title', 'default']);
+        });
+    });
+
+    describe('apply types', () => {
+        it('should take a function', () => {
+            var spec = {
+                getPath: 'proveedores',
+                apply: proveedores => proveedores.map(p => p.nombre),
+                setPath: 'schema.properties.proveedores.items.enum'
+            };
+            var obj = hoseFit(spec, input);
+            assert.equal(obj.schema.properties.proveedores.items.enum[0], 'pipi');
+        });
+        it('should take an array [args, body] ', () => {
+            var spec = {
+                getPath: 'proveedores',
+                apply: [ 'proveedores', 'return proveedores.map(p => p.id)' ],
+                setPath: 'schema.properties.proveedores.items.enum'
+            };
+            var obj = hoseFit(spec, input);
+            assert.equal(obj.schema.properties.proveedores.items.enum[0], 1414);
+        });
+        it('should take an object { args , body }', () => {
+            var spec = {
+                getPath: 'proveedores',
+                apply: { args: 'proveedores', body: 'return proveedores.map(p => p.id)' },
+                setPath: 'schema.properties.proveedores.items.enum'
+            };
+            var obj = hoseFit(spec, input);
+            assert.equal(obj.schema.properties.proveedores.items.enum[0], 1414);
+        });
+    });
+});
